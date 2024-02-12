8240b220fd3f089f3f16d83652fca0f30082d489
Michael Chandler <michaelchandler@wifi-10-43-117-243.wifi.berkeley.edu>
2024-02-12 11:14:05 -0800

    Homework 2

---
+++ I_am_going_to_be_rich/my_bank_account.py
@@ -0,0 +1,4 @@
+print(1290+24980*(242894**21*48)*8*4)
+#The name of the file is my bank account because this arithmetic problem I formulated is what I hope to be in my bank acount someday so 
+#I chose a number so large that money wouldn't be an issue for me in the future.")
+
